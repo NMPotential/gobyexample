da00df962293f712108910fdcab3642e652bbb9a
NMPotential <nmpotential@NMPotential.local>
2019-06-03 10:26:32 +0100

    Added HelloWorld and Values files

---
+++ Values/values.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//STRINGS - CAN BE ADDED TOGETHER WITH +
+	fmt.Println("go" + "lang")
+
+	//INTEGERS
+	fmt.Println("1+1 =", 1+1)
+
+	//FLOATS
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	//BOOLEANS
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
